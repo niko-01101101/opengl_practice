29b1d7a7e26daddedd6f3b416e008aea82db18bc
nik <niko.01101101@gmail.com>
2024-09-23 18:33:19 -0500

    Almost working build

---
+++ Tetris/build/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/src/main.cpp.o"
+  "CMakeFiles/Tetris.dir/src/main.cpp.o.d"
+  "Tetris.app/Contents/MacOS/Tetris"
+  "Tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
